b0a4e17da346a1a1da425980da703966f9a830a6
Tim Rowe <tim@tjsr.id.au>
2024-07-27 12:12:16 +1000

    Update depbot to try to fix token.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { ExpressServerHelper } from './expressHelper.js';
 export type { ExpressServerConfig } from './types/expressConfig.js';
+export type { UserSessionOptions } from '@tjsr/user-session-middleware';
+export { getMysqlSessionStore } from '@tjsr/user-session-middleware';
